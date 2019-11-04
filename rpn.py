b4cf3400056a9d8960f12da7cf170e5d405a66f6
ptse <ptse@umich.edu>
2019-11-03 20:26:09 -0500

    Add test for subtraction

--- rpn.py
+++ rpn.py
@@ -8,12 +8,17 @@ def calculate(string):
          arg2 = stack.pop()
          result = arg1 + arg2
          stack.append(result)
+      elif token == '-':
+         arg1 = stack.pop()
+         arg2 = stack.pop()
+         result = arg1 - arg2
+         stack.append(result)
       else:
          stack.append(int(token))
 
    
-   print(stack)
-
+      print(stack)
+   return stack.pop()
 
 def main():
    while True:
--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,9 @@ class TestBasics(unittest.TestCase):
    def test_add(self):
       result = rpn.calculate('1 1 +')
       self.assertEqual(2, result)
+   def test_sub(self):
+      result = rpn.calculate(5 3 -)
+      self.asserEqual(2, result)
+   def test_badinput(self):
+      with self.aserRaises(TypeError):
+         rpn.calculate('1 2 3 +')
