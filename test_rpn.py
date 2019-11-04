a8ab537426545a6fd9a29c4de80d7fbf4384903e
ptse <ptse@umich.edu>
2019-11-04 01:33:15 -0500

    changed rpn and tests

--- test_rpn.py
+++ test_rpn.py
@@ -1,16 +1,18 @@
 import unittest
+
 import rpn
 
 class TestBasics(unittest.TestCase):
-   def test_add(self):
-      result = rpn.calculate('1 1 +')
-      self.assertEqual(2, result)
-   def test_add(self):
-      result = rpn.calculate('1 1 +')
-      self.assertEqual(2, result)
-   def test_sub(self):
-      result = rpn.calculate(5 3 -)
-      self.asserEqual(2, result)
-   def test_badinput(self):
-      with self.aserRaises(TypeError):
-         rpn.calculate('1 2 3 +')
+    def test_add(self):
+        result = rpn.calculate("1 1 +")
+        self.assertEqual(2, result)
+    def test_subtract(self):
+        result = rpn.calculate("5 3 -")
+        self.assertEqual(2, result)
+    def test_multiply(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
+    def test_divide(self):
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
+
